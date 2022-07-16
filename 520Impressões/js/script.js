bf72ded8ab9269eb2338754babac63eeceaed9df
rodrigosousa28 <sousarodrigofreitas07@gmail.com>
2022-07-16 16:38:53 -0300

    Animações adicionadas ao longo do site

--- 520Impressões/js/script.js
+++ 520Impressões/js/script.js
@@ -7,3 +7,21 @@ function show(){
         mobileMenu.classList.add('open');
     }
 }
+
+let target = document.querySelectorAll('[data-animation]');
+let animationClass = 'animate';
+
+function animeScroll(){
+    let windowTop = window.pageYOffset + ((window.innerHeight * 3)/4);
+    target.forEach(function(element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+window.addEventListener('scroll', function(){
+    animeScroll();
+})

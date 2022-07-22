aa26ca6f0c632fb8b32be993daf03dabd2e2e977
rodrigosousa28 <sousarodrigofreitas07@gmail.com>
2022-07-21 21:33:58 -0300

    Desktop menu dropdown adicionado

--- Enterprise/script.js
+++ Enterprise/script.js
@@ -1,4 +1,36 @@
-function deskmenu(){
-    const t = document.getElementsByClassName('menu');
-    console.log(t);
+function deskmenu1(){
+    const t = document.querySelector('.dropdown-menu-1');
+    if (t.classList.contains('open')){
+        t.classList.remove('open');
+    }else{
+        t.classList.add('open')
+    }
+
+    const k = document.getElementById('mc1');
+    if (k.classList.contains('menu-closed')){
+        k.classList.remove('menu-closed');
+        k.classList.add('menu-open');
+    }else{
+        k.classList.remove('menu-open');
+        k.classList.add('menu-closed');
+    }
+    
+}
+
+function deskmenu2(){
+    const l = document.querySelector('.dropdown-menu-2');
+    if (l.classList.contains('open')){
+        l.classList.remove('open');
+    }else{
+        l.classList.add('open')
+    }
+
+    const r = document.getElementById('mc2');
+    if (r.classList.contains('menu-closed')){
+        r.classList.remove('menu-closed');
+        r.classList.add('menu-open');
+    }else{
+        r.classList.remove('menu-open');
+        r.classList.add('menu-closed');
+    }
 }

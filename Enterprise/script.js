3c4eac6fb2bd5a0f4bd1e8d5fe96f193310de210
rodrigosousa28 <sousarodrigofreitas07@gmail.com>
2022-07-23 17:42:58 -0300

    Otimização no JS
    Redução na quantidade de funções

--- Enterprise/script.js
+++ Enterprise/script.js
@@ -1,12 +1,12 @@
-function deskmenu1(){
-    const t = document.querySelector('.dropdown-menu-1');
+function deskmenu(id, classe){
+    const t = document.querySelector(classe);
     if (t.classList.contains('open')){
         t.classList.remove('open');
     }else{
         t.classList.add('open')
     }
 
-    const k = document.getElementById('mc1');
+    const k = document.getElementById(id);
     if (k.classList.contains('menu-closed')){
         k.classList.remove('menu-closed');
         k.classList.add('menu-open');
@@ -14,29 +14,10 @@ function deskmenu1(){
         k.classList.remove('menu-open');
         k.classList.add('menu-closed');
     }
-    
 }
 
-function deskmenu2(){
-    const l = document.querySelector('.dropdown-menu-2');
-    if (l.classList.contains('open')){
-        l.classList.remove('open');
-    }else{
-        l.classList.add('open')
-    }
-
-    const r = document.getElementById('mc2');
-    if (r.classList.contains('menu-closed')){
-        r.classList.remove('menu-closed');
-        r.classList.add('menu-open');
-    }else{
-        r.classList.remove('menu-open');
-        r.classList.add('menu-closed');
-    }
-}
-
-function openMenuMob1(){
-    const z = document.getElementById('mc11');
+function openMenuMob(id1, id2){
+    const z = document.getElementById(id1);
     if (z.classList.contains('menu-closed-mob')){
         z.classList.remove('menu-closed-mob');
         z.classList.add('menu-open-mob');
@@ -45,33 +26,13 @@ function openMenuMob1(){
         z.classList.add('menu-closed-mob');
     }
 
-    const b = document.getElementById('menumob1');
+    const b = document.getElementById(id2);
     if (b.classList.contains('mobile-menu-item-closed')){
         b.classList.remove('mobile-menu-item-closed');
         b.classList.add('mobile-menu-item-open')
     }else{
         b.classList.remove('mobile-menu-item-open');
         b.classList.add('mobile-menu-item-closed')
-    }
-}
-
-function openMenuMob2(){
-    const y = document.getElementById('mc12');
-    if (y.classList.contains('menu-closed-mob')){
-        y.classList.remove('menu-closed-mob');
-        y.classList.add('menu-open-mob');
-    }else{
-        y.classList.remove('menu-open-mob');
-        y.classList.add('menu-closed-mob');
-    }
-
-    const a = document.getElementById('menumob2');
-    if (a.classList.contains('mobile-menu-item-closed')){
-        a.classList.remove('mobile-menu-item-closed');
-        a.classList.add('mobile-menu-item-open')
-    }else{
-        a.classList.remove('mobile-menu-item-open');
-        a.classList.add('mobile-menu-item-closed')
     }
 }
 
